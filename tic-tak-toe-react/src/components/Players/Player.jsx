b3f4aaac68e0f5defcf0ebd7bbeeed251c78387a
Nadav <nadav.shalit@gmail.com>
2024-01-17 09:58:35 +0200

    support Required & disabled empty player name

--- tic-tak-toe-react/src/components/Players/Player.jsx
+++ tic-tak-toe-react/src/components/Players/Player.jsx
@@ -9,8 +9,16 @@ export default function Player({
   const [playerName, setPlayerName] = useState(`Player ${idx + 1}`);
   const [isEditMode, setIsEditMode] = useState(false);
   // console.log("Player.jsx", { activePlayer, symbol });
-  function handleClick() {
-    setIsEditMode((editMode) => !editMode);
+  function onInputFocus(e) {
+    e.target.select();
+  }
+  function handleNameClick() {
+    handleButtonClick();
+  }
+  function handleButtonClick() {
+    setIsEditMode((editMode) => {
+      return !editMode;
+    });
   }
   function handleInput(e) {
     // console.log({ target: e.target, val: e.target.value });
@@ -19,21 +27,31 @@ export default function Player({
   }
   return (
     <li className={activePlayer === symbol ? "active" : ""}>
+      <span className="player-symbol">{symbol}</span>
       <span className="player">
         {isEditMode ? (
-          <input
-            placeholder={playerName}
-            value={playerName}
-            onInput={handleInput}
-            type="text"
-            required
-          ></input>
+          <span>
+            <input
+              placeholder={playerName}
+              value={playerName}
+              // onLoad={onInputLoad}
+              onFocus={onInputFocus}
+              onInput={handleInput}
+              type="text"
+              required
+            ></input>
+          </span>
         ) : (
-          <span className="player-name">{playerName}</span>
+          <span className="player-name" onClick={handleNameClick}>
+            {playerName}
+          </span>
         )}
-        <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={handleClick}>{isEditMode ? "Save" : "Edit"}</button>
+
+      <button disabled={!playerName} onClick={handleButtonClick}>
+        {isEditMode ? "Save" : "Edit"}
+      </button>
+      {!playerName && <h5>Required!</h5>}
     </li>
   );
 }
