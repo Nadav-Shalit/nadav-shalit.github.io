bc9e247acc2365f06b5ebd40d5c8382be3eb963e
Nadav <nadav.shalit@gmail.com>
2024-01-29 14:30:14 +0200

    max header solution

--- investment_calculator_react/src/componenet/Header/Header.jsx
+++ investment_calculator_react/src/componenet/Header/Header.jsx
@@ -1,7 +1,10 @@
+import logo from "../../assets/investment-calculator-logo.png";
+
 export default function Header() {
   return (
-    <div id="header">
+    <header id="header">
+      <img src={logo} alt="React Investment Calculator" />
       <h1>React Investment Calculator</h1>
-    </div>
+    </header>
   );
 }
