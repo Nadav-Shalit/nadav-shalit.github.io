d0c2d6cc0c22db15fb31139e4743640e3fc9e85d
Nadav <nadav.shalit@gmail.com>
2024-02-14 16:50:22 +0200

    fix more path to img

--- seatgeek-api/src/componenets/Api-Data/Taxonomies/TaxonomyMenu.jsx
+++ seatgeek-api/src/componenets/Api-Data/Taxonomies/TaxonomyMenu.jsx
@@ -1,4 +1,4 @@
-import imgSrc from "../../../assets/seatgeek-def_280-210.png";
+import imgSrc from "https://www.ndv.co.il/react-proj/seatgeek-api/assets/seatgeek-def_280-210.png";
 export default function TaxonomyMenu({ data, filter, parentId }) {
   const classesActive =
     "inline-block p-2 bg-red-500 text-black rounded-t-lg active  ";
